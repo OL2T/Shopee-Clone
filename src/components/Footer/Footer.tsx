234c1554d775f3453faaa4f088dc2d04e06bd722
ol2t <lamtantai2000@gmail.com>
2024-05-16 00:37:56 +0700

    Update for deployment to production

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 import FooterAbove from './FooterAbove'
 import FooterBellow from './FooterBellow'
-interface Props {
-  children?: React.ReactNode
-}
-export default function Footer({ children }: Props) {
+// interface Props {
+//   children?: React.ReactNode
+// }
+export default function Footer() {
   return (
     <footer className='pt-top50 bg-neutral-100 text-black/54 text-sm'>
       <div className='container max-w-container mx-auto'>
