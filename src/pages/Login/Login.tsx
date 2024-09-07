ba64714fd22acc790649d6abe792082ea712beee
Lâm Tài <lamtai@192.168.1.6>
2024-09-08 00:39:33 +0700

    Update UI login/register page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,65 @@
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className='bg-redRegister'>
+      <div className='w-1024 bg-register-hero-pattern h-600 mx-auto relative'>
+        <div className='absolute -translate-y-[50%] top-[50%] w-full right-0'>
+          {/* <Helmet>
+          <title>Đăng nhập | Shopee Clone</title>
+          <meta name='description' content='Đăng nhập vào dự án Shopee Clone' />
+        </Helmet> */}
+          <div className='container'>
+            <div className='grid grid-cols-1 py-12 lg:grid-cols-5'>
+              <div className='lg:col-span-2 lg:col-start-4'>
+                <form
+                  className='rounded bg-white p-10 shadow-sm'
+                  // onSubmit={onSubmit}
+                  noValidate
+                >
+                  <div className='text-2xl'>Đăng Nhập</div>
+
+                  <input
+                    name='email'
+                    // register={register}
+                    type='email'
+                    className=' mt-8 bg-white px-4 py-2 border placeholder-gray-400 border-gray-300 w-full focus:outline-none focus:ring-1 focus:ring-gray-900 placeholder:text-sm'
+                    // errorMessage={errors.email?.message}
+                    placeholder='Email/Số điện thoại/Tên đăng nhập'
+                  />
+
+                  <input
+                    name='password'
+                    // register={register}
+                    type='password'
+                    className='mt-[30px] bg-white px-4 py-2 border placeholder-gray-400 border-gray-300 w-full focus:outline-none focus:ring-1 focus:ring-gray-900 placeholder:text-sm'
+                    // classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                    // errorMessage={errors.password?.message}
+                    placeholder='Password'
+                    autoComplete='on'
+                  />
+                  <div className='mt-8'>
+                    <button
+                      type='submit'
+                      className='flex  w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'
+                      // isLoading={loginMutation.isLoading}
+                      // disabled={loginMutation.isLoading}
+                    >
+                      Đăng nhập
+                    </button>
+                  </div>
+                  <div className='mt-8 flex items-center justify-center'>
+                    <span className='text-gray-400 text-sm'>
+                      Bạn mới biết đến Shopee?
+                    </span>
+                    <a className='text-sm ml-1 text-red-400' href='/register'>
+                      Đăng ký
+                    </a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,3 +1,39 @@
 export default function ProductList() {
-  return <div>ProductList</div>
+  return (
+    <div className='bg-gray-200 py-6'>
+      {/* <Helmet>
+        <title>Trang chủ | Shopee Clone</title>
+        <meta name='description' content='Trang chủ dự án Shopee Clone' />
+      </Helmet>
+      <div className='container'>
+        {productsData && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3'>
+              <AsideFilter
+                queryConfig={queryConfig}
+                categories={categoriesData?.data.data || []}
+              />
+            </div>
+            <div className='col-span-9'>
+              <SortProductList
+                queryConfig={queryConfig}
+                pageSize={productsData.data.data.pagination.page_size}
+              />
+              <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                {productsData.data.data.products.map((product) => (
+                  <div className='col-span-1' key={product._id}>
+                    <Product product={product} />
+                  </div>
+                ))}
+              </div>
+              <Pagination
+                queryConfig={queryConfig}
+                pageSize={productsData.data.data.pagination.page_size}
+              />
+            </div>
+          </div>
+        )}
+      </div> */}
+    </div>
+  )
 }
