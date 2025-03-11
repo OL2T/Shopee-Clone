5d86c76310e18263763a5fcddae2cc111a277f77
Lâm Tài <lamtantai2000@gmail.com>
2025-03-11 23:43:29 +0700

    Add product list

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -5,6 +5,7 @@ import SortProductList from './components/SortProductList/SortProductList'
 import { useQuery } from '@tanstack/react-query'
 import useQueryParams from 'src/hooks/useQueryParams'
 import productAPI from 'src/apis/product.api'
+import Product from './components/Product/Product'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
@@ -14,6 +15,8 @@ export default function ProductList() {
       return productAPI.getProducts(queryParams)
     }
   })
+
+  const productsData = data.data || { data: { data: { products: [] } } }
 
   const [searchParams] = useSearchParams()
   const queryConfig = useMemo(() => {
@@ -58,11 +61,11 @@ export default function ProductList() {
             // pageSize={productsData.data.data.pagination.page_size}
             />
             <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {/* {productsData.data.data.products.map((product) => (
+              {productsData.data?.data?.products?.map((product) => (
                 <div className='col-span-1' key={product._id}>
                   <Product product={product} />
                 </div>
-              ))} */}
+              )) || []}
             </div>
             {/* <Pagination
               queryConfig={queryConfig}
