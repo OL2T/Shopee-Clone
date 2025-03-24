a085bdfd89a174f97dc24162e5aa8009ad508e2c
Lâm Tài <lamtantai2000@gmail.com>
2025-03-25 00:08:54 +0700

    Handle error when expired access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { ToastContainer } from 'react-toastify'
 import useRouteElement from './useRouteElement'
 import 'react-toastify/dist/ReactToastify.css'
+import { useContext, useEffect } from 'react'
+import { localStorageEventTarget } from './types/auth'
+import { AppContext } from './Contexts/app.context'
 
 function App() {
+  const { reset } = useContext(AppContext)
   const routeElements = useRouteElement()
+
+  useEffect(() => {
+    localStorageEventTarget.addEventListener('clearLocalStorage', reset)
+
+    return () => {
+      localStorageEventTarget.removeEventListener('clearLocalStorage', reset)
+    }
+  }, [reset])
+
   return (
     <>
       <div className='bg-neutral-100'>
